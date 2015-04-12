6cd4d917be51ab6c514e5cd413b15547b92d6cd6
Vivek Subedi <shree665@gmail.com>
2015-04-12 15:02:04 -0400

    added icon

--- src/subedi/address/MainApp.java
+++ src/subedi/address/MainApp.java
@@ -10,6 +10,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -45,6 +46,9 @@ public class MainApp extends Application {
 		
 		this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/addressIcon.png"));
 
         initRootLayout();
 
